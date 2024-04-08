4b3a09c97b346aed102bfe0790bd838f76d4e488
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-04-08 16:09:21 +0530

    Add project files.

---
+++ NZWalks-api/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAllStudents() {
+
+            string[] studentNames = new string[] { "John", "ASh", "Mark", "dolu", "bolu" };
+            return Ok(studentNames);
+        }
+    }
+}
